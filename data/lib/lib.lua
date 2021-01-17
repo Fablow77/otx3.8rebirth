26078e0cd1cefb0d4d018e4ce4dc5ffa87309ec5
Fablow <jacobszabo2016@outlook.com>
2021-01-17 14:04:01 -0800

    Add files via upload

---
+++ data/lib/lib.lua
@@ -0,0 +1,13 @@
+-- Core API functions implemented in Lua
+dofile('data/lib/core/core.lua')
+
+-- Compatibility library for our old Lua API
+dofile('data/lib/compat/compat.lua')
+
+-- Custom
+dofile('data/lib/custom/custom.lua')
+dofile('data/lib/custom/storages.lua')
+dofile('data/lib/custom/rewardboss.lua')
+dofile('data/lib/custom/achievements.lua')
+dofile('data/lib/custom/killing_in_the_name_of.lua')
+dofile('data/lib/custom/dailyTask.lua')
